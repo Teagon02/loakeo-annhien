bffc6eec3281a4d433eac041a60a51d229c22fd4
Nguyễn Phạm Khanh <teagon@MacBook-Pro-Teagons.local>
2025-12-11 14:48:59 +0700

    fix bug

--- app/api/addresses/route.ts
+++ app/api/addresses/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth, currentUser } from "@clerk/nextjs/server";
-import { serverWriteClient } from "@/sanity/lib/server-client";
+import { serverWriteClient } from "@/sanity/lib/client";
 
 interface AddressRequestBody {
   fullName: string;
