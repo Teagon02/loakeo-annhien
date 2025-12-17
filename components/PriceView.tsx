17a1fe351b7851ec77d2bc1792e994a25c36d39e
Nguyễn Phạm Khanh <teagon@MacBook-Pro-Teagons.local>
2025-12-17 15:43:02 +0700

    fix UI price format

--- components/PriceView.tsx
+++ components/PriceView.tsx
@@ -8,13 +8,13 @@ interface Props {
 }
 const PriceView = ({ price, discount, className }: Props) => {
   return (
-    <div className="flex flex-wrap items-center gap-1.5">
-      <PriceFormatter amount={price} className="text-shop_dark_green" />
+    <div className="flex items-center gap-1 whitespace-nowrap">
+      <PriceFormatter amount={price} className="text-sm text-shop_dark_green" />
       {price && discount ? (
         <>
           <PriceFormatter
             amount={price + (price * discount) / 100}
-            className="text-shop_light_text line-through font-normal"
+            className="text-sm text-shop_light_text line-through font-normal"
           />
         </>
       ) : null}
